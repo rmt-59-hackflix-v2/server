eb4c4e69db764d5a9d504ef7b15379bb9bbd7461
Fachri Hawari <fachri.hawari@gmail.com>
2025-04-08 13:30:24 +0700

    feat: Implement root route with controller

--- app.js
+++ app.js
@@ -8,9 +8,7 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.get('/', (req, res) => {
-  res.send('Welcome to the API');
-});
+app.use('/', require('./router/index.router'));
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
